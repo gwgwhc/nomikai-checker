707483f6412b0c67401ea6aade780cd710a4c97f
gwgwhc <georgehudsonchang@gmail.com>
2024-10-24 10:40:50 +0900

    added MANIFEST.in to solve images/ not being found

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 
-# Read the README file for a long description (optional but recommended)
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="nomikai_checker",
-    version="4.0.0",
+    version="4.0.1",
     author="gwgwhc",
     description="Check if it's time for nomikai - if the Gods will it.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/gwgwhc/nomikai-checker",
     packages=find_packages(),
-    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    include_package_data=True,
     entry_points={
     'console_scripts': [
         'nomikai_checker=nomikai_checker.__main__:main',
     ],
     },
     install_requires=[
-        "PySimpleGUI>=4.0",  # External dependencies required for the project
+        "PySimpleGUI<=4.60.5", # version without license issues
     ],
     classifiers=[
         "Programming Language :: Python :: 3", 
